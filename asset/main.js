bcde980e230fbd4e5fadfc7df0fca0786d7d7644
Clément Long <clmt.long@gmail.com>
2016-10-23 17:58:44 +0200

    feat(gulp): Init gulp with babel, sass, autoprefixer

---
+++ asset/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var test = 'test';
+
+console.log(test);
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-ruby-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+// Babel ♥️
+gulp.task('babel', () => {
+  return gulp.src('scripts/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('asset'));
+});
+
+// Sass ♥️
+gulp.task('sass', () => {
+  sass('styles/*.scss')
+    .on('error', sass.logError)
+    .pipe(autoprefixer({
+        browsers: ['> 1%'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('asset'))
+});
+
+// Watch
+gulp.task('watch', function () {
+  gulp.watch('styles/*.scss', ['sass']);
+  gulp.watch('scripts/*.js', ['babel']);
+});
+
+// Dev
+gulp.task('dev', ['sass', 'babel']);
+
+// Prod
+gulp.task('prod', ['sass', 'babel', 'autoprefixer']);
+
+// Default
+gulp.task('default', ['dev']);
